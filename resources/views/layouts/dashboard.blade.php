457d825967b03cc6fc3f3cf2da432fb8cc0b3cf6
Alejandro Martinez <ing.amartinez94@gmail.com>
2018-05-16 14:16:47 -0500

    css y js importados.

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -24,22 +24,22 @@
     <link href="{{asset('assets/css/pages.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/css/menu.css')}}" rel="stylesheet" type="text/css" />
     <link href="{{asset('assets/css/responsive.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/jquery.timepicker.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/css/bootstrap-datepicker3.standalone.min.css')}}" rel="stylesheet" type="text/css" />
+    {{--<link href="{{asset('assets/css/jquery.timepicker.css')}}" rel="stylesheet" type="text/css" />--}}
+    {{--<link href="{{asset('assets/css/bootstrap-datepicker3.standalone.min.css')}}" rel="stylesheet" type="text/css" />--}}
     <link href="{{asset('assets/plugins/bootstrap-sweetalert/sweetalert2.css')}}" rel="stylesheet" type="text/css" />
 
     <!-- App css -->
     <link href="{{asset('assets/plugins/datatables/jquery.dataTables.min.css')}}" rel="stylesheet" type="text/css" />
-    <link href="{{asset('assets/plugins/toastr/toastr.css')}}" rel="stylesheet" type="text/css" />
+    {{--<link href="{{asset('assets/plugins/toastr/toastr.css')}}" rel="stylesheet" type="text/css" />--}}
 
     <!-- HTML5 Shiv and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
-    <![endif]-->
+    <!--<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>-->
+    {{--<script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>--}}
+    {{--<![endif]-->--}}
 
-    <script src="{{asset('assets/js/modernizr.min.js')}}"></script>
+    {{--<script src="{{asset('assets/js/modernizr.min.js')}}"></script>--}}
 
 </head>
 
@@ -168,18 +168,18 @@
 <script src="{{asset('assets/js/fastclick.js')}}"></script>
 <script src="{{asset('assets/js/jquery.blockUI.js')}}"></script>
 <script src="{{asset('assets/js/waves.js')}}"></script>
-<script src="{{asset('assets/js/jquery.nicescroll.js')}}"></script>
+{{--<script src="{{asset('assets/js/jquery.nicescroll.js')}}"></script>--}}
 <script src="{{asset('assets/js/jquery.slimscroll.js')}}"></script>
-<script src="{{asset('assets/js/jquery.scrollTo.min.js')}}"></script>
+{{--<script src="{{asset('assets/js/jquery.scrollTo.min.js')}}"></script>--}}
 
-<!-- KNOB JS -->
-<!--[if IE]>
-<script type="text/javascript" src="{{asset('assets/plugins/jquery-knob/excanvas.js')}}"></script>
-<![endif]-->
-<script src="{{asset('assets/plugins/jquery-knob/jquery.knob.js')}}"></script>
+{{--<!-- KNOB JS -->--}}
+{{--<!--[if IE]>--}}
+{{--<script type="text/javascript" src="{{asset('assets/plugins/jquery-knob/excanvas.js')}}"></script>--}}
+{{--<![endif]-->--}}
+{{--<script src="{{asset('assets/plugins/jquery-knob/jquery.knob.js')}}"></script>--}}
 
 <!--Chart.js-->
-<script src="{{asset('assets/plugins/chart.js/dist/Chart.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/chart.js/dist/Chart.js')}}"></script>--}}
 
 <!-- Datatable -->
 <script src="{{asset('assets/plugins/datatables/jquery.dataTables.min.js')}}"></script>
@@ -196,19 +196,19 @@
 <script type="text/javascript" src="{{asset('assets/js/validator.js')}}"></script>
 
 <script src="{{asset('assets/js/sha3.min.js')}}"></script>
-<script src="{{asset('assets/js/jquery.formdata.js')}}"></script>
+{{--<script src="{{asset('assets/js/jquery.formdata.js')}}"></script>--}}
 
 <script src="{{asset('assets/js/moment-with-locales.js')}}"></script>
-<script src="{{asset('assets/js/livestamp.js')}}"></script>
+{{--<script src="{{asset('assets/js/livestamp.js')}}"></script>--}}
 
-<script src="{{asset('assets/js/jquery.timepicker.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap-datepicker.min.js')}}"></script>
-<script src="{{asset('assets/js/bootstrap-datepicker.es.min.js')}}"></script>
+{{--<script src="{{asset('assets/js/jquery.timepicker.js')}}"></script>--}}
+{{--<script src="{{asset('assets/js/bootstrap-datepicker.min.js')}}"></script>--}}
+{{--<script src="{{asset('assets/js/bootstrap-datepicker.es.min.js')}}"></script>--}}
 
-<script src="{{asset('assets/js/arduino.js')}}"></script>
+{{--<script src="{{asset('assets/js/arduino.js')}}"></script>--}}
 {{--<script src="{{asset('assets/js/masonry.pkgd.min.js')}}"></script>--}}
 <script src="{{asset('assets/js/isotope.pkgd.min.js')}}"></script>
-<script src="{{asset('assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js')}}"></script>
+{{--<script src="{{asset('assets/plugins/bootstrap-inputmask/bootstrap-inputmask.min.js')}}"></script>--}}
 
 @stack('script')
 
