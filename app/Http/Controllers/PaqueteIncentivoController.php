c48e40d5636f5bb1b0cd267a636f7959ca0e91e5
Alejandro Martinez <ing.amartinez94@gmail.com>
2018-06-08 09:49:31 -0500

    Paquete 4gb

--- app/Http/Controllers/PaqueteIncentivoController.php
+++ app/Http/Controllers/PaqueteIncentivoController.php
@@ -630,6 +630,10 @@ class PaqueteIncentivoController extends Controller
                         $paquete["paquete_id"]="datos";
                         break;
 
+                    case "4096":
+                        $paquete["paquete_id"]="datos";
+                        break;
+
                     default:
                         $paquete["paquete_id"]=null;
                         break;
