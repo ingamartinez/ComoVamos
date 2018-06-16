102d4629fb60c0d52b2176496642f35aa36f44f0
Alejandro Martinez <ing.amartinez94@gmail.com>
2018-06-16 11:49:50 -0500

    Ajuste consulta detalle por asesor

--- app/Http/Controllers/ResumenController.php
+++ app/Http/Controllers/ResumenController.php
@@ -32,10 +32,6 @@ class ResumenController extends Controller
         }
 
         if (isset($supervisor_r)){
-
-        }
-
-        if (isset($supervisor_r)){
             $resumenPorPaquete= \DB::table('users')
                 ->join('paquetes_incentivos',"paquetes_incentivos.users_id",'users.id')
                 ->join('users as supervisores',"users.users_id",'supervisores.id')
@@ -136,17 +132,30 @@ class ResumenController extends Controller
      */
     public function detallePaquetesAsesor(Request $request)
     {
+        $fechaInicial=$request->input('fechaInicial');
+        $fechaFinal=$request->input('fechaFinal');
+
+        if (!isset($fechaInicial) or !isset($fechaInicial)){
+            $fechaInicial=Carbon::now()->startOfDay()->toDateTimeString();
+            $fechaFinal=Carbon::now()->endOfDay()->toDateTimeString();
+        }else{
+            $fechaInicial=Carbon::parse($fechaInicial)->startOfDay()->toDateTimeString();
+            $fechaFinal=Carbon::parse($fechaFinal)->endOfDay()->toDateTimeString();
+        }
+
         $resumenPorPaquete= \DB::table('paquetes_incentivos')
             ->select(
                 'paquetes_incentivos.paquete',
                 \DB::raw('Sum(paquetes_incentivos.valor) as valor'),
                 \DB::raw('Count(paquetes_incentivos.movil) as cantidad')
             )
-            ->where('paquetes_incentivos.created_at','>=',$request->fechaInicial)
-            ->where('paquetes_incentivos.created_at','<=',$request->fechaFinal)
+            ->where('paquetes_incentivos.created_at','>=',$fechaInicial)
+            ->where('paquetes_incentivos.created_at','<=',$fechaFinal)
             ->where('paquetes_incentivos.users_id','=',$request->user)
             ->groupBy('paquetes_incentivos.paquete')
             ->get();
+
+//        dd($resumenPorPaquete,$request->all());
         return response()->json($resumenPorPaquete,202);
     }
     /**
