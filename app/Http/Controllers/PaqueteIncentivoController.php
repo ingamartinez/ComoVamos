d89ab87c06ac60219ce97bf7bd8195a7e79f18a9
Alejandro Martinez <ing.amartinez94@gmail.com>
2018-07-03 08:18:09 -0500

    Validar paquete mes anterior

--- app/Http/Controllers/PaqueteIncentivoController.php
+++ app/Http/Controllers/PaqueteIncentivoController.php
@@ -588,10 +588,10 @@ class PaqueteIncentivoController extends Controller
             $paquete_mes_anterior=false;
 
             $sorted->each(function ($item, $key) use (&$paquete_mes_anterior){
-//                if($item["fecha"]->month==Carbon::now()->month-1){
-//                    $paquete_mes_anterior = false;
+                if($item["fecha"]->month==Carbon::now()->month-1){
+                    $paquete_mes_anterior = false;
 
-//                }else
+                }else
                 if($item["fecha"]->month!=Carbon::now()->month){
                     $paquete_mes_anterior = true;
                     return false;
